ea32af51294f4853d09a9b4f7130332c19579057
Raser23 <ser.neshadim@yandex.ru>
2017-08-29 12:52:11 +0700

    unit hp added

--- GamedevBudetUdivlen/Assets/Scripts/UnitStats.cs
+++ GamedevBudetUdivlen/Assets/Scripts/UnitStats.cs
@@ -1,6 +1,23 @@
 ﻿using UnityEngine;
 
-public class UnitStats : MonoBehaviour {
+public class UnitStats : MonoBehaviour
+{
     public float speed;
     public float rotateSpeed;
+    public float maxHp;
+
+    float _hp;
+
+    private void Start()
+    {
+        
+        _hp = maxHp;
+        //HP -= 10;
+        //print(HP);
+    }
+    public float HP
+    {
+        get { return _hp; }
+        set { _hp = value; }
+    }
 }
