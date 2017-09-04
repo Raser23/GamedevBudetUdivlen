5add6a3e5abc361e0d6ef95f8964a83da69d3df0
Andronchanskiy <31404779+Andronchanskiy@users.noreply.github.com>
2017-09-04 18:45:06 +0700

    45
    gef

--- GamedevBudetUdivlen/Assets/Scripts/TowerPlaceholder.cs
+++ GamedevBudetUdivlen/Assets/Scripts/TowerPlaceholder.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class TowerPlaceholder : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Camera cam;
+    bool Has_Tower = false;
+    // Use this for initialization
+    void Start () {
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnMouseDown() {
+        if (!Has_Tower)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    CreateTower();
+                }
+            }
+        }
+    }
+
+    void CreateTower()
+    {
+        print("Menu appears");
+        GameObject Tower = Instantiate(Resources.Load("Assault Tower (nice)", typeof(GameObject))) as GameObject;
+        Tower.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 2);
+
+        Has_Tower = true;
+    }
 }
