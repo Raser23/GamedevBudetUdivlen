31416329ae731f95bdeb8035c78c4b6b2767dbd4
Yaroslav <kharchenko.yar@gmail.com>
2017-09-17 12:04:28 +0700

    a

---
+++ GamedevBudetUdivlen/Assets/Scripts/UI/WaveCount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCount : MonoBehaviour {
+
+	public Text waves;
+	void Update () {
+		string mw = GameManager.instance.spawner.maxWaveCount.ToString();
+		string cw = GameManager.instance.spawner.currentWaveNum.ToString();
+		waves.text = cw + "/" + mw;
+	}
+}
