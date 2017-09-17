5f36077ddedb1e73bd7f739a45570a05eea24c05
Yaroslav <kharchenko.yar@gmail.com>
2017-09-17 13:23:43 +0700

    noice

--- GamedevBudetUdivlen/Assets/Scripts/Towers/AssaultTower.cs
+++ GamedevBudetUdivlen/Assets/Scripts/Towers/AssaultTower.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VolumetricLines;
 
 public class AssaultTower : Tower {
 
 	public Transform rotor;
 	public float rotSpeed;
 	public Color gizmColor;
-	 
+	public VolumetricLineBehavior laser;
+	public Transform muzzle;
 
 	
 
@@ -31,12 +33,15 @@ public class AssaultTower : Tower {
 		Quaternion newRotation = Quaternion.LookRotation(rotor.position - obj.transform.position,Vector3.forward);
 		newRotation.x = 0;
 		newRotation.y = 0;
-		rotor.rotation = Quaternion.Lerp(rotor.rotation, newRotation, Time.deltaTime * 8);
+		rotor.rotation = Quaternion.Lerp(rotor.rotation, newRotation, Time.deltaTime * rotSpeed);
 	}
 
 	void Update () {
 		if(currentTarget){
+			laser.gameObject.SetActive(true);
 			float dist = Vector3.Distance(currentTarget.transform.position, self_position);
+			Vector3 d  = currentTarget.transform.position - muzzle.position;
+			laser.EndPos = new Vector3(0, Mathf.Sqrt(Mathf.Pow(d.x, 2) + Mathf.Pow(d.y, 2))/muzzle.transform.lossyScale.y, d.z/muzzle.transform.lossyScale.y);
 			if(dist>range){
 				currentTarget = null;
 			}
@@ -53,6 +58,7 @@ public class AssaultTower : Tower {
 
 		}
 		else{
+			laser.gameObject.SetActive(false);
 			lookForTarget();
 		}
 	}
